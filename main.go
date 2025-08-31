bacf259975244908b1606eb2bd2172781eaeb29e
JuaniCurtale <jcurtale@alumnos.exa.unicen.edu.ar>
2025-08-30 22:15:58 -0300

    se agregaron comentarios

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 
 func handlerDescrip(w http.ResponseWriter, r *http.Request) {
 
+	//Se chequea si el path no es el indicado, de no serlo la pagina arroja error 404
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
+	//Se sirve el HTML index.html "Se enlaza"
 	http.ServeFile(w, r, "index.html")
+
+	//Se establecera la cabecera
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
 
